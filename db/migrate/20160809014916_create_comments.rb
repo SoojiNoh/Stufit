3d425bf4ae82f5aee79c8a7f8d4584d207c20b21
jaeseong <ljs8860@snu.ac.kr>
2016-08-09 10:56:32 +0900

    view file 생성d

---
+++ db/migrate/20160809014916_create_comments.rb
@@ -0,0 +1,12 @@
+class CreateComments < ActiveRecord::Migration[5.0]
+  def change
+    create_table :comments do |t|
+
+      t.string :content
+      t.references :user, index:true, foreign_key: true
+      t.references :event, indext: true, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
