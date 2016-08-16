5bb632b5fe21865cd9254228d3fa1fadb7012fb5
jjjjihye <potere22@naver.com>
2016-08-16 17:13:25 +0000

    first commit

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,43 @@
 class EventsController < ApplicationController
+    
+    def index
+        @events=Event.all
+    end
+
+    def new
+        @event=Event.new
+    end
+    
+    def create
+        Event.create(event_params)
+        @event = Event.new            
+        redirect_to events_path
+    end
+    
+    def show
+        @event=Event.find(params[:id])
+    end
+    
+    def edit
+        @event=Event.find(params[:id])
+    end
+    
+    def update
+        @event=Event.find(params[:id])
+        @event.update(event_params)
+        
+        redirect_to event_path(@event)
+    end
+    
+    def destroy
+        @event=Event.find(params[:id])
+        @event.destroy
+        redirect_to events_path
+    end
+
+    private
+    
+    def event_params
+        params.require(:event).permit(:title, :content, :created_at, :place, :file, :event_type, :start_at, :end_at)
+    end
 end
