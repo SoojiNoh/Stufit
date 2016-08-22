462dc2bd407744fd34ca2b78102bd6dada466f47
jjjjihye <potere22@naver.com>
2016-08-22 10:19:07 +0000

    버튼수정

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,7 @@ class EventsController < ApplicationController
     
     def index
         @events=Event.all
-        @events =Event.paginate(:page => params[:page], :per_page => 2)
+        @events =Event.reorder("created_at DESC").paginate(:page => params[:page], :per_page => 10)
     end
 
     def new
