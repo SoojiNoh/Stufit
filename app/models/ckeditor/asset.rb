b3fab12c7b8a0f85ff8cbe8698483ab6b6027531
jaeseong <ljs8860@snu.ac.kr>
2016-09-05 16:42:25 +0900

    .

--- app/models/ckeditor/asset.rb
+++ app/models/ckeditor/asset.rb
@@ -1,10 +1,5 @@
 class Ckeditor::Asset < ActiveRecord::Base
-  #include ActiveRecord
-  #include Ckeditor::Orm::ActiveRecord::AssetBase
-  #include Ckeditor::Backend::Carrierwave
+  include ActiveRecord
   include Ckeditor::Orm::ActiveRecord::AssetBase
-
-  delegate :url, :current_path, :content_type, :to => :data
-
-  validates_presence_of :data
+  include Ckeditor::Backend::CarrierWave
 end
