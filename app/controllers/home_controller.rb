6f8f89424fa3976f9380c1b9042975edacac635e
soojinoh <thensj@hanmail.net>
2016-09-06 10:45:26 +0000

    한번더 푸쉬~

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,10 +6,10 @@ class HomeController < ApplicationController
     @today =Date.today.to_s
     
     if !user_signed_in?
-      @task_name = "앞으로의 활동들"
+      @task_name = "앞으로의 활동"
       @tasks = Event.where("start_at> ? ", "%#{@today}%")
     else
-      @task_name = "내가 해야할 것들"
+      @task_name = "내가 해야할 것"
       @tasks = current_user.events.all
     end
     
