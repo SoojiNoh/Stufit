53e0fd584de11524765f2a776df581b2f2d33ccf
soojinoh <thensj@hanmail.net>
2016-09-06 09:55:56 +0000

    home의 my task 칸 연동중

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,21 @@
 class HomeController < ApplicationController
   skip_before_action :authenticate_user!
+  require 'date'
+  
   def index
+    @today =Date.today.to_s
+    
+    if user_signed_in?
+      @task_name = "앞으로의 활동들"
+      @tasks = Event.where("start_at> ? ", "%#{@today}%")
+    else
+      @task_name = "내가 해야할 것들"
+      @tasks = current_user.events.all
+      @tasks << current_user.schedules.all
+    end
+    
+    @tasks.order(end_at: :desc)
+    
   end
 
 end
