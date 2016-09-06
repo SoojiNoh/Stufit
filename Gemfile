57017abb6dec22d5166462fda960ad1a05477cd3
soojinoh <thensj@hanmail.net>
2016-09-06 18:23:45 +0000

    carousel 우선입힘 gem owlcarousel-rails

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'owlcarousel-rails'
 gem 'ckeditor'
 gem "paperclip"
 gem 'jquery-turbolinks'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,4 +19,5 @@
 //= require fullcalendar
 //= require ckeditor/init
 //= require turbolinks
+//= require owl.carousel
 $.turbo.use('pjax:start', 'pjax:end');
