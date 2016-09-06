a563e903a681a4314243695c313cb70565450a5a
soojinoh <thensj@hanmail.net>
2016-09-06 10:27:47 +0000

    로그인 로그아웃 액션 달고 my task 연동 에러 안정화

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,13 +5,12 @@ class HomeController < ApplicationController
   def index
     @today =Date.today.to_s
     
-    if user_signed_in?
+    if !user_signed_in?
       @task_name = "앞으로의 활동들"
       @tasks = Event.where("start_at> ? ", "%#{@today}%")
     else
       @task_name = "내가 해야할 것들"
       @tasks = current_user.events.all
-      @tasks << current_user.schedules.all
     end
     
     @tasks.order(end_at: :desc)
