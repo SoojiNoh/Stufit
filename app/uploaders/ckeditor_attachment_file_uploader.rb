e55e802d455c53396383eaa91486a215ccccb7fc
likelion-wku2831 <musiclace@naver.com>
2016-09-11 03:54:35 +0000

    .

--- app/uploaders/ckeditor_attachment_file_uploader.rb
+++ app/uploaders/ckeditor_attachment_file_uploader.rb
@@ -10,12 +10,12 @@ class CkeditorAttachmentFileUploader < CarrierWave::Uploader::Base
   # include CarrierWave::ImageScience
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/ckeditor/attachments/#{model.id}"
+    "uploads/"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
--- app/uploaders/ckeditor_picture_uploader.rb
+++ app/uploaders/ckeditor_picture_uploader.rb
@@ -4,16 +4,16 @@ class CkeditorPictureUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or ImageScience support:
   # include CarrierWave::RMagick
-  include CarrierWave::MiniMagick
+  # include CarrierWave::MiniMagick
   # include CarrierWave::ImageScience
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/ckeditor/pictures/#{model.id}"
+    "uploads/"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
@@ -31,13 +31,7 @@ class CkeditorPictureUploader < CarrierWave::Uploader::Base
   process :extract_dimensions
 
   # Create different versions of your uploaded files:
-  version :thumb do
-    process resize_to_fill: [118, 100]
-  end
 
-  version :content do
-    process resize_to_limit: [800, 800]
-  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
