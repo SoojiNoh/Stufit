d59aaff01f7f4ffa0235b0fdbce80f06d95a593c
SoojiNoh <thensj@hanmail.net>
2016-09-14 06:33:47 +0000

    slack연동테스트1

---
+++ app/helpers/testtest_helper.rb
@@ -0,0 +1,2 @@
+module TesttestHelper
+end
---
+++ test/controllers/testtest_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class TesttestControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
