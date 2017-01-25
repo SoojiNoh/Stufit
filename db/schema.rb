2f56b08f8df142a5ec0bac719220bd3482ff736e
heejaykong <heejaykong@naver.com>
2017-01-25 07:49:12 +0000

    푸터색상, 로고 크기 등

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20161230024701) do
 
-  create_table "activities", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "activities", force: :cascade do |t|
     t.string   "activity_type"
     t.string   "style"
     t.string   "host_name"
@@ -27,27 +27,27 @@ ActiveRecord::Schema.define(version: 0) do
     t.integer  "user_id"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
-    t.index ["user_id"], name: "index_activities_on_user_id", using: :btree
+    t.index ["user_id"], name: "index_activities_on_user_id"
   end
 
-  create_table "articles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "articles", force: :cascade do |t|
     t.string   "title"
-    t.text     "body",       limit: 65535
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "badges_sashes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "badges_sashes", force: :cascade do |t|
     t.integer  "badge_id"
     t.integer  "sash_id"
     t.boolean  "notified_user", default: false
     t.datetime "created_at"
-    t.index ["badge_id", "sash_id"], name: "index_badges_sashes_on_badge_id_and_sash_id", using: :btree
-    t.index ["badge_id"], name: "index_badges_sashes_on_badge_id", using: :btree
-    t.index ["sash_id"], name: "index_badges_sashes_on_sash_id", using: :btree
+    t.index ["badge_id", "sash_id"], name: "index_badges_sashes_on_badge_id_and_sash_id"
+    t.index ["badge_id"], name: "index_badges_sashes_on_badge_id"
+    t.index ["sash_id"], name: "index_badges_sashes_on_sash_id"
   end
 
-  create_table "ckeditor_assets", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "ckeditor_assets", force: :cascade do |t|
     t.string   "data_file_name",               null: false
     t.string   "data_content_type"
     t.integer  "data_file_size"
@@ -59,11 +59,11 @@ ActiveRecord::Schema.define(version: 0) do
     t.integer  "height"
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
-    t.index ["assetable_type", "assetable_id"], name: "idx_ckeditor_assetable", using: :btree
-    t.index ["assetable_type", "type", "assetable_id"], name: "idx_ckeditor_assetable_type", using: :btree
+    t.index ["assetable_type", "assetable_id"], name: "idx_ckeditor_assetable"
+    t.index ["assetable_type", "type", "assetable_id"], name: "idx_ckeditor_assetable_type"
   end
 
-  create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "comments", force: :cascade do |t|
     t.string   "content"
     t.integer  "user_id"
     t.integer  "event_id"
@@ -73,15 +73,15 @@ ActiveRecord::Schema.define(version: 0) do
     t.integer  "community_id"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
-    t.index ["community_id"], name: "index_comments_on_community_id", using: :btree
-    t.index ["consult_id"], name: "index_comments_on_consult_id", using: :btree
-    t.index ["event_id"], name: "index_comments_on_event_id", using: :btree
-    t.index ["major_story_id"], name: "index_comments_on_major_story_id", using: :btree
-    t.index ["university_id"], name: "index_comments_on_university_id", using: :btree
-    t.index ["user_id"], name: "index_comments_on_user_id", using: :btree
+    t.index ["community_id"], name: "index_comments_on_community_id"
+    t.index ["consult_id"], name: "index_comments_on_consult_id"
+    t.index ["event_id"], name: "index_comments_on_event_id"
+    t.index ["major_story_id"], name: "index_comments_on_major_story_id"
+    t.index ["university_id"], name: "index_comments_on_university_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
-  create_table "communities", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "communities", force: :cascade do |t|
     t.string   "title"
     t.string   "content"
     t.string   "image"
@@ -90,35 +90,35 @@ ActiveRecord::Schema.define(version: 0) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "community_likes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "community_likes", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "community_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
-    t.index ["community_id"], name: "index_community_likes_on_community_id", using: :btree
-    t.index ["user_id"], name: "index_community_likes_on_user_id", using: :btree
+    t.index ["community_id"], name: "index_community_likes_on_community_id"
+    t.index ["user_id"], name: "index_community_likes_on_user_id"
   end
 
-  create_table "consults", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "consults", force: :cascade do |t|
     t.string   "title"
-    t.text     "content",    limit: 65535
+    t.text     "content"
     t.string   "image"
     t.integer  "user_id"
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-    t.index ["user_id"], name: "index_consults_on_user_id", using: :btree
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_consults_on_user_id"
   end
 
-  create_table "event_likes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "event_likes", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "event_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["event_id"], name: "index_event_likes_on_event_id", using: :btree
-    t.index ["user_id"], name: "index_event_likes_on_user_id", using: :btree
+    t.index ["event_id"], name: "index_event_likes_on_event_id"
+    t.index ["user_id"], name: "index_event_likes_on_user_id"
   end
 
-  create_table "events", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "events", force: :cascade do |t|
     t.string   "title"
     t.string   "content"
     t.string   "place"
@@ -133,66 +133,258 @@ ActiveRecord::Schema.define(version: 0) do
     t.integer  "university_id"
     t.datetime "created_at",                        null: false
     t.datetime "updated_at",                        null: false
-    t.index ["university_id"], name: "index_events_on_university_id", using: :btree
-    t.index ["user_id"], name: "index_events_on_user_id", using: :btree
+    t.index ["university_id"], name: "index_events_on_university_id"
+    t.index ["user_id"], name: "index_events_on_user_id"
   end
 
-  create_table "favorite_events", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "favorite_events", force: :cascade do |t|
     t.string   "event_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "frontobjects", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "frontobjects", force: :cascade do |t|
     t.integer  "frontable_id"
     t.string   "frontable_type"
     t.string   "image"
     t.string   "link_url"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
-    t.index ["frontable_type", "frontable_id"], name: "index_frontobjects_on_frontable_type_and_frontable_id", using: :btree
+    t.index ["frontable_type", "frontable_id"], name: "index_frontobjects_on_frontable_type_and_frontable_id"
   end
 
-  create_table "hash_activities", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "hash_activities", id: false, force: :cascade do |t|
     t.integer  "hash_tag_id"
     t.integer  "activity_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.index ["activity_id"], name: "index_hash_activities_on_activity_id", using: :btree
-    t.index ["hash_tag_id"], name: "index_hash_activities_on_hash_tag_id", using: :btree
+    t.index ["activity_id"], name: "index_hash_activities_on_activity_id"
+    t.index ["hash_tag_id"], name: "index_hash_activities_on_hash_tag_id"
   end
 
-  create_table "hash_events", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "hash_events", id: false, force: :cascade do |t|
     t.integer  "event_id"
     t.integer  "hash_tag_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.index ["event_id"], name: "index_hash_events_on_event_id", using: :btree
-    t.index ["hash_tag_id"], name: "index_hash_events_on_hash_tag_id", using: :btree
+    t.index ["event_id"], name: "index_hash_events_on_event_id"
+    t.index ["hash_tag_id"], name: "index_hash_events_on_hash_tag_id"
   end
 
-  create_table "hash_majors", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "hash_majors", id: false, force: :cascade do |t|
     t.integer  "hash_tag_id"
     t.integer  "major_story_id"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
-    t.index ["hash_tag_id"], name: "index_hash_majors_on_hash_tag_id", using: :btree
-    t.index ["major_story_id"], name: "index_hash_majors_on_major_story_id", using: :btree
+    t.index ["hash_tag_id"], name: "index_hash_majors_on_hash_tag_id"
+    t.index ["major_story_id"], name: "index_hash_majors_on_major_story_id"
   end
 
-  create_table "hash_tags", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "hash_tags", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "identities", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "identities", force: :cascade do |t|
     t.integer  "user_id"
     t.string   "provider"
     t.string   "uid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_identities_on_user_id", using: :btree
+    t.index ["user_id"], name: "index_identities_on_user_id"
+  end
+
+  create_table "impressions", force: :cascade do |t|
+    t.string   "impressionable_type"
+    t.integer  "impressionable_id"
+    t.integer  "user_id"
+    t.string   "controller_name"
+    t.string   "action_name"
+    t.string   "view_name"
+    t.string   "request_hash"
+    t.string   "ip_address"
+    t.string   "session_hash"
+    t.text     "message"
+    t.text     "referrer"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["controller_name", "action_name", "ip_address"], name: "controlleraction_ip_index"
+    t.index ["controller_name", "action_name", "request_hash"], name: "controlleraction_request_index"
+    t.index ["controller_name", "action_name", "session_hash"], name: "controlleraction_session_index"
+    t.index ["impressionable_type", "impressionable_id", "ip_address"], name: "poly_ip_index"
+    t.index ["impressionable_type", "impressionable_id", "request_hash"], name: "poly_request_index"
+    t.index ["impressionable_type", "impressionable_id", "session_hash"], name: "poly_session_index"
+    t.index ["impressionable_type", "message", "impressionable_id"], name: "impressionable_type_message_index"
+    t.index ["user_id"], name: "index_impressions_on_user_id"
+  end
+
+  create_table "major_stories", force: :cascade do |t|
+    t.string   "univ"
+    t.string   "major"
+    t.string   "grade"
+    t.string   "name"
+    t.string   "file"
+    t.text     "q1"
+    t.text     "q2"
+    t.text     "q3"
+    t.text     "q4"
+    t.text     "q5"
+    t.text     "q6"
+    t.integer  "user_id"
+    t.integer  "university_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.index ["university_id"], name: "index_major_stories_on_university_id"
+    t.index ["user_id"], name: "index_major_stories_on_user_id"
+  end
+
+  create_table "major_story_likes", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "major_story_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.index ["major_story_id"], name: "index_major_story_likes_on_major_story_id"
+    t.index ["user_id"], name: "index_major_story_likes_on_user_id"
+  end
+
+  create_table "merit_actions", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "action_method"
+    t.integer  "action_value"
+    t.boolean  "had_errors",    default: false
+    t.string   "target_model"
+    t.integer  "target_id"
+    t.text     "target_data"
+    t.boolean  "processed",     default: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "merit_activity_logs", force: :cascade do |t|
+    t.integer  "action_id"
+    t.string   "related_change_type"
+    t.integer  "related_change_id"
+    t.string   "description"
+    t.datetime "created_at"
+  end
+
+  create_table "merit_score_points", force: :cascade do |t|
+    t.integer  "score_id"
+    t.integer  "num_points", default: 0
+    t.string   "log"
+    t.datetime "created_at"
+  end
+
+  create_table "merit_scores", force: :cascade do |t|
+    t.integer "sash_id"
+    t.string  "category", default: "default"
+  end
+
+  create_table "paththroughs", force: :cascade do |t|
+    t.string   "way"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "paththroughs_users", id: false, force: :cascade do |t|
+    t.integer "paththrough_id", null: false
+    t.integer "user_id",        null: false
+    t.index ["paththrough_id", "user_id"], name: "index_paththroughs_users_on_paththrough_id_and_user_id"
+    t.index ["user_id", "paththrough_id"], name: "index_paththroughs_users_on_user_id_and_paththrough_id"
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string   "name"
+    t.string   "resource_type"
+    t.integer  "resource_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
+    t.index ["name"], name: "index_roles_on_name"
+  end
+
+  create_table "sashes", force: :cascade do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "schedules", force: :cascade do |t|
+    t.string   "title"
+    t.string   "content"
+    t.string   "place"
+    t.string   "file"
+    t.string   "event_type"
+    t.datetime "start_at"
+    t.datetime "end_at"
+    t.boolean  "done",          default: false
+    t.integer  "university_id"
+    t.integer  "user_id"
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
+    t.index ["university_id"], name: "index_schedules_on_university_id"
+    t.index ["user_id"], name: "index_schedules_on_user_id"
+  end
+
+  create_table "universities", force: :cascade do |t|
+    t.string   "name"
+    t.string   "img"
+    t.string   "link_url"
+    t.string   "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "university_follows", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "university_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.index ["university_id"], name: "index_university_follows_on_university_id"
+    t.index ["user_id"], name: "index_university_follows_on_user_id"
+  end
+
+  create_table "user_events", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_user_events_on_event_id"
+    t.index ["user_id"], name: "index_user_events_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.boolean  "admin"
+    t.string   "image"
+    t.string   "membertype"
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "name"
+    t.string   "highschool"
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.integer  "sash_id"
+    t.integer  "level",                  default: 0
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "users_roles", id: false, force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "role_id"
+    t.index ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id"
   end
 
 end
